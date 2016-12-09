6f9612dd699955ef37924a6f734cb467dbacef05
Kevin P. Fleming <kevin@km6g.us>
2016-12-08 19:23:35 -0500

    Remove obsolete files

--- launch-vpc-student.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-aws cloudformation create-stack --stack-name student01 --template-body file:///Users/kpfleming/jepsen/jepsen-vpc-student.json --parameters file:///Users/kpfleming/jepsen/jepsen-vpc-student-parameters.json
