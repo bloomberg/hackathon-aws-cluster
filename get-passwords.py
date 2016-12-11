9d9808340d539206bbb233197c909b5a18c1f019
Kevin P. Fleming <kevin@km6g.us>
2016-12-11 09:08:15 -0500

    Add script to obtain passwords from S3 bucket Remove 'public-read' permissions from S3 bucket Add 'concurrent' and 's3transfer' Python modules

---
+++ get-passwords.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys
+import os
+import boto3
+
+region = 'eu-west-1'
+
+cf = boto3.client(region_name = region, service_name = 'cloudformation')
+
+st = cf.describe_stacks(StackName = 'jepsen')
+
+outputs = st['Stacks'][0]['Outputs']
+
+for out in outputs:
+    if out['Description'] == 'Bucket':
+        bucket = out['OutputValue']
+
+s3 = boto3.client(region_name = region, service_name = 's3')
+
+list = s3.list_objects_v2(Bucket = bucket, Prefix = 'passwd.')
+
+if not os.path.exists('passwords'):
+    os.makedirs('passwords')
+
+for obj in list['Contents']:
+    print("Getting", obj['Key'])
+    with open('passwords/' + obj['Key'] + '.txt', 'w') as pwfile:
+        s3.download_fileobj(bucket, obj['Key'], pwfile)
