0ad84be3dae471412f5e25a26da8be8947c7347e
Kevin P. Fleming <kevin@km6g.us>
2017-02-23 12:50:03 -0500

    Fixes to get shellinabox working

--- gen_proxies.sh
+++ gen_proxies.sh
@@ -3,8 +3,8 @@
 for n in `seq $1 $2`
 do
   cat <<EOF
-ProxyPass "/student$n/shell" "http://192.168.$n.100:4200"
-ProxyPassReverse "/student$n/shell" "http://192.168.$n.100:4200"
+ProxyPass "/student$n/shell" "http://192.168.$n.100:80"
+ProxyPassReverse "/student$n/shell" "http://192.168.$n.100:80"
 <Location /student$n/jupyter>
     ProxyPass "http://192.168.$n.100:8080"
     ProxyPassReverse "http://192.168.$n.100:8080"
